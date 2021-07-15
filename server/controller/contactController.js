691ffb10add40d1de4357665bdb785cc843ed27a
brahim-lqati <brahim98lqati@gmail.com>
2021-07-15 15:59:15 +0100

    setup repo

--- server/controller/contactController.js
+++ server/controller/contactController.js
@@ -1,2 +1,8 @@
 const asyncHandler = require("express-async-handler");
 const Contact = require('../models/Contact');
+const C
+exports.store = (req, res) => {
+    const {name, sujet, email, content} = req.body;
+
+    
+}
