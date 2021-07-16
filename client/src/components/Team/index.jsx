7f2720506919f9f04114803af5d749fa02ea095b
MahmoudFettal <46266986+MahmoudFettal@users.noreply.github.com>
2021-07-16 12:29:38 +0100

    small fixs

--- client/src/components/Team/index.jsx
+++ client/src/components/Team/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 // import { makeStyles } from '@material-ui/core/styles';
 // import AppBar from '@material-ui/core/AppBar';
 // import Toolbar from '@material-ui/core/Toolbar';
@@ -7,13 +7,12 @@ import React from 'react';
 // import IconButton from '@material-ui/core/IconButton';
 // import MenuIcon from '@material-ui/icons/Menu';
 
-import styles from './style.module.css'
- import Slider from "react-slick";
- import "slick-carousel/slick/slick.css"; 
- import "slick-carousel/slick/slick-theme.css";
+import styles from "./style.module.css";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Team = () => {
-  
   const settings = {
     dots: true,
     infinite: true,
@@ -22,31 +21,23 @@ const Team = () => {
     autoplay: true,
     speed: 2000,
     autoplaySpeed: 2000,
-    cssEase: "linear"
+    cssEase: "linear",
   };
 
   return (
-      <div>
-            <Slider {...settings}>
-    <div className={styles.card}>
-    <div className={styles.desc}>
-      <div className={styles.name}>
-        Brahim <span className={styles.lname}>Elqati</span>
-      </div>
-      <div className={styles.team}>Developement Team</div>
+    <div>
+      <Slider {...settings}>
+        <div className={styles.card}>
+          <div className={styles.desc}>
+            <div className={styles.name}>
+              Brahim <span className={styles.lname}>Elqati</span>
+            </div>
+            <div className={styles.team}>Developement Team</div>
+          </div>
+        </div>
+      </Slider>
     </div>
-  </div>
-  <div className={styles.card}>
-    <div className={styles.desc}>
-      <div className={styles.name}>
-        Brahim <span className={styles.lname}>Elqati</span>
-      </div>
-      <div className={styles.team}>Developement Team</div>
-    </div>
-  </div>
-  </Slider>
-      </div>
   );
-}
+};
 
 export default Team;
