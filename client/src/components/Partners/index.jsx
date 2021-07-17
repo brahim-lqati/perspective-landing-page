bc9a4fd184de75d0eacda42243a21da9616455fe
MahmoudFettal <Mahmoud.fettal@gmail.com>
2021-07-17 14:07:14 +0100

    version 1.2

--- client/src/components/Partners/index.jsx
+++ client/src/components/Partners/index.jsx
@@ -5,7 +5,7 @@ import orange from  "./logo-orange.jpg";
 function Partners() {
     return (
     <div className={styles.parteners}>
-        <h2 className={styles.sectionTitle}>Our Collaboratif Partners</h2>
+        <h2 className={styles.sectionTitle}>Our Amazing Partners</h2>
         <hr className={styles.hr} />
         <div className={styles.list}>
             <img className={styles.logo} src={orange} alt="Orange logo" />
