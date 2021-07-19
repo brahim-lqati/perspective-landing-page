35f3089117c2cfeb7073702b22aea6ccce6b45b1
brahim-lqati <brahim98lqati@gmail.com>
2021-07-19 11:46:27 +0100

    add some modif on navbar

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -2,39 +2,45 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import styles from "./style.module.css";
 import { Nav, Navbar } from "react-bootstrap";
-
+import React, {useState} from "react";
 function Navigation() {
+  const [scroll, setScroll] = useState(false);
+  const changeBackground = () => {
+    window.scrollY >= 80 ? setScroll(true) : setScroll(false);
+  }
+  window.addEventListener('scroll', changeBackground);
+
   return (
-    <Navbar className={styles.nav} collapseOnSelect expand="lg">
+    <Navbar className={scroll ?styles.navActive  : styles.nav } collapseOnSelect expand="lg" sticky="top">
       <Navbar.Brand href="#home">
         <img
           className={styles.logo}
-          src={"logo/perception.svg"}
+          src={scroll ? 'logo/perception_white.svg' : 'logo/perception.svg'}
           alt="perception"
         />
       </Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse className={styles.links}>
         <Nav>
-          <a className={styles.link} href="#">
+          <a className={scroll ? [styles.link, styles.test].join(" ") : styles.link} href="#">
             Home
           </a>
-          <a className={styles.link} href="#mission">
+          <a className={scroll ? [styles.link, styles.test].join(" ") : styles.link} href="#mission">
             Mission
           </a>
-          <a className={styles.link} href="#products">
+          <a className={scroll ? [styles.link, styles.test].join(" ") : styles.link} href="#products">
             Products
           </a>
-          <a className={styles.link} href="#team">
+          <a className={scroll ? [styles.link, styles.test].join(" ") : styles.link} href="#team">
             Team
           </a>
-          <a className={styles.link} href="#partners">
+          <a className={scroll ? [styles.link, styles.test].join(" ") : styles.link} href="#partners">
             Paterns
           </a>
-          <a className={styles.link} href="/contact">
+          <a className={scroll ? [styles.link, styles.test].join(" ") : styles.link} href="/contact">
             Contact Us
           </a>
-          <a className={styles.btnbuy} href="#">
+          <a className={scroll ? [styles.btnbuy, styles.btnbuyActive].join(" ") : styles.btnbuy} href="#">
             Buy
           </a>
         </Nav>
