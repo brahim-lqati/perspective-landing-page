2287f99329425142f50c0898354c20ba9208009b
brahim-lqati <brahim98lqati@gmail.com>
2021-07-19 11:57:51 +0100

    v1 host

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.mongoURI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    });
-  } catch (error) {
-    process.exit(1);
-  }
-};
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.mongoURI, {
+//       useUnifiedTopology: true,
+//       useNewUrlParser: true,
+//     });
+//   } catch (error) {
+//     process.exit(1);
+//   }
+// };
 
-module.exports = connectDB;
+// module.exports = connectDB;
