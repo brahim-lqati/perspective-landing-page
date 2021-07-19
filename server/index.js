f53a08c1b88c623fafd11e3158687a33c2d0d14a
brahim-lqati <brahim98lqati@gmail.com>
2021-07-19 12:51:01 +0100

    v3

--- server/index.js
+++ server/index.js
@@ -3,12 +3,20 @@ const path = require('path');
 const app = express();
 require('dotenv').config();
 
-app.use(express.static(path.resolve(__dirname, "./client/build")));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use("/api",require('./routes/api/contact.routes'));
+
+const __direname = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__direname, '/client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, '..', 'client', 'build', 'index.html'),
+    );
+  });
+}
 
 app.listen(process.env.PORT || 5000, () => {
     console.log(`server running in port ${process.env.PORT}`);
